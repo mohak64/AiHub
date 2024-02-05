db2454f553ead37ecaa266671b76360800c63c6c
mohak64 <mohak64bansal@gmail.com>
2024-02-05 11:05:19 +0530

    create page starting

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,23 +1,8 @@
-import { fetchLLM } from "../action";
-
-import LlmCard, { LlmProp } from "@/components/LlmCard";
-import LoadMore from "../../components/LoadMore";
-// import { data } from "./_data";
+import LoadMore from "@/components/LoadMore";
 
 async function CreatePage() {
-  const data = await fetchLLM(1);
-
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
-      <h2 className="text-3xl text-white font-bold">
-        Explore AI models and LLMs
-      </h2>
-
-      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: LlmProp, index: number) => (
-          <LlmCard key={item.id} llm={item} index={index} />
-        ))}
-      </section>
       <LoadMore />
     </main>
   );
